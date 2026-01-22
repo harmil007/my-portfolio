9d19c4612fed94ec569c106227cd45a5e1245069
Harmil Goti <harrygoti0@gmail.com>
2026-01-22 23:20:48 +0530

    Improved SEO stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,22 @@ import { ThemeProvider } from "next-themes";
 export const metadata = {
   title: "Harmil Goti | Frontend Developer",
   description:
-    "Frontend Developer with 3+ years of experience in React.js and Next.js, building scalable, high-performance web applications.",
+    "Harmil Goti is a Frontend Developer with 3+ years of experience in React.js and Next.js, building scalable and high-performance web applications.",
+  keywords: [
+    "Harmil Goti",
+    "Frontend Developer",
+    "React.js",
+    "Next.js",
+    "Portfolio",
+  ],
+  authors: [
+    { name: "Harmil Goti", url: "https://harmil-portfolio.netlify.app" },
+  ],
   openGraph: {
     title: "Harmil Goti | Frontend Developer",
     description:
-      "React & Next.js developer focused on scalable, high-quality web applications.",
-    url: "https://your-domain.vercel.app",
+      "React & Next.js developer focused on building scalable, high-quality web applications.",
+    url: "https://harmil-portfolio.netlify.app",
     siteName: "Harmil Goti Portfolio",
     images: [
       {
@@ -20,6 +30,14 @@ export const metadata = {
       },
     ],
     type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Harmil Goti | Frontend Developer",
+    description:
+      "Frontend Developer with expertise in React.js & Next.js, creating high-performance web apps.",
+    images: ["/og.png"],
+    creator: "@harmilgoti",
   },
 };
 
